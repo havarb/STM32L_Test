0026d1a3fc85c60a0dcbbe4c04b2593d69835d1b
havarb <havarb@gmail.com>
2019-02-06 13:42:02 +0100

    FirstCode
    The project is created here.

---
+++ Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.d
@@ -0,0 +1,89 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Inc/stm32l4xx_hal_conf.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Inc/stm32l4xx_hal_conf.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+C:/Users/havar.bathaee.GREENPACK/Documents/L4_test/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
